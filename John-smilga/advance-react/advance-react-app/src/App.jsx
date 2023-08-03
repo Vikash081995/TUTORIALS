3b969734a12af2ce1a99366a9d1a27d4498a4908
Vikash081995 <Vikash08.Kumar1995@gmail.com>
2023-08-03 14:06:12 +0530

    first commit

---
+++ John-smilga/advance-react/advance-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h2>Advanced React</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ John-smilga/useState/02-useState-basics.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const UseStateBasics = () => {
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    // console.log(count)
+    setCount(count + 1);
+    // be careful, we can set any value
+    // setCount('pants');
+  };
+  return (
+    <div>
+      <h4>You clicked {count} times</h4>
+      <button className="btn" onClick={handleClick}>
+        Click me
+      </button>
+    </div>
+  );
+};
+
+export default UseStateBasics;
