28cb48882976a2f66e05f678d9f8515ce9a25ad6
Vikash081995 <Vikash08.Kumar1995@gmail.com>
2024-02-26 22:32:01 +0530

    position property project

--- John-smilga/mern-stack-app/src/pages/Landing.jsx
+++ John-smilga/mern-stack-app/src/pages/Landing.jsx
@@ -1,11 +1,47 @@
+import styled from "styled-components";
+import logo from "../assets/logo.svg";
+import main from "../assets/main.svg";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing</h1>
-
-    </div>
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            Job <span>tracking</span>
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum
+            maxime delectus, dolorem voluptatem placeat, dicta qui soluta sed
+            deleniti at excepturi voluptatibus porro in rem et ratione pariatur
+            animi odit?
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login/ Demo user
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img"/>
+      </div>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  background: red;
+  h1 {
+    color: white;
+  }
+  .content {
+    background: blue;
+    color: yellow;
+  }
+`;
 
 export default Landing;
