48a8b73967e8e1b113cf7ab66fe4b6ada295f029
Vikash081995 <Vikash08.Kumar1995@gmail.com>
2024-04-08 21:51:01 +0530

    union props

--- Academind/React-and-Typescript-The-practical-guide/react-ts-basics/src/components/CourseGoalList.tsx
+++ Academind/React-and-Typescript-The-practical-guide/react-ts-basics/src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
 import CourseGoal from "./CourseGoal";
 import { type CourseGoal as CourseGoalType } from "../App";
+import InfoBox from "./InfoBox";
+import { ReactNode } from "react";
 
 type CourseGoalListProps = {
   goals: CourseGoalType[];
@@ -7,16 +9,37 @@ type CourseGoalListProps = {
 };
 
 const CourseGoalList = ({ goals, onDelete }: CourseGoalListProps) => {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">
+        You have no course goals yet, Start adding some!
+      </InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning" severity="low">
+        lot of goals{" "}
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal id={goal.id} title={goal.title} onDelete={onDelete}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDelete}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
