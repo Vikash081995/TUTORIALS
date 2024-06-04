7d3f85f7a19d5d106ae695ebf4187c9b64c8a278
Vikash081995 <Vikash08.Kumar1995@gmail.com>
2024-06-04 22:05:15 +0530

    today nextjs

--- Academind/NEXTJS14-AND-REACT/05-onwards-foodies-starting-project/app/meals/Page.js
+++ Academind/NEXTJS14-AND-REACT/05-onwards-foodies-starting-project/app/meals/Page.js
@@ -1,6 +1,11 @@
+import { Suspense } from "react";
 import MealsGrid from "../components/meals/meals-grid";
 import classes from "./page.module.css";
 
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 export default async function MealsPage() {
   const meals = await getMeals();
   return (
@@ -16,7 +21,11 @@ export default async function MealsPage() {
         <Link href="/meals/share">share your fav recipe</Link>
       </p>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
