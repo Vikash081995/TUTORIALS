4d3e7b4652c5220b50c82f02581913450be64152
Vikash081995 <Vikash08.Kumar1995@gmail.com>
2024-07-20 02:19:39 +0530

    uncontrolled flow

---
+++ Udemy/REACTJS-REDUX-ALL/react-design-patterns-advanced/controlled-uncontrolled-pattern/src/App.js
@@ -0,0 +1,38 @@
+import { UncontrolledFlow } from "./components/uncontrolled-flow";
+
+const StepOne = ({ goNext }) => {
+  return (
+    <>
+      <h1>Step #1</h1>
+      <button onClick={goNext}>Next</button>
+    </>
+  );
+};
+const StepTwo = ({ goNext }) => {
+  return (
+    <>
+      <h1>Step #1</h1>
+      <button onClick={goNext}>Next</button>
+    </>
+  );
+};
+const StepThree = ({ goNext }) => {
+  return (
+    <>
+      <h1>Step #1</h1>
+      <button onClick={goNext}>Next</button>
+    </>
+  );
+};
+
+function App() {
+  return (
+    <UncontrolledFlow>
+      <StepOne />
+      <StepTwo />
+      <StepThree />
+    </UncontrolledFlow>
+  );
+}
+
+export default App;
