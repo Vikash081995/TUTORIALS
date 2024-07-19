df84563d1fe8973f6366e8b8fad6fd23cfa597a1
Vikash081995 <Vikash08.Kumar1995@gmail.com>
2024-07-19 16:32:57 +0530

    modal

--- Udemy/REACTJS/react-design-patterns-advanced/src/App.js
+++ Udemy/REACTJS/react-design-patterns-advanced/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
-
+import Modal from "./components/Modal";
+import { LargeBookListItem } from "./reusable-lists/books/LargeListItems";
+import { books } from "./reusable-lists/data/books";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Modal>
+      <LargeBookListItem book={books[0]} />
+    </Modal>
+  );
 };
 
 export default App;
