6ec870f2ec20aa10635bab0b01907ea2760ca247
Vikash081995 <Vikash08.Kumar1995@gmail.com>
2024-07-26 19:23:32 +0530

    del

--- Udemy/All-html-css-js/50WEB-PROJECTS-50DAYS/DAY1-SIGN-UP-FORM/main.js
+++
@@ -1,6 +0,0 @@
-const container = document.querySelector(".container");
-const signUpButton = document.querySelector(".green-bg button");
-
-signUpButton.addEventListener("click", () => {
-  container.classList.toggle("change");
-});
