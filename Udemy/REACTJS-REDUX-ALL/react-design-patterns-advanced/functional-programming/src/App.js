47fcabe53ab5e91b79b68aead106696cada7bfad
Vikash081995 <Vikash08.Kumar1995@gmail.com>
2024-07-27 14:07:50 +0530

    functional programming

---
+++ Udemy/REACTJS-REDUX-ALL/react-design-patterns-advanced/functional-programming/src/App.js
@@ -0,0 +1,11 @@
+import { RedButton, SmallRedButton } from "./components/partial";
+function App() {
+  return (
+    <>
+      <RedButton text="I am red" />
+      <SmallRedButton text="i am small red button" />
+    </>
+  );
+}
+
+export default App;
